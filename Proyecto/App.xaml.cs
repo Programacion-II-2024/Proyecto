7a39bec5807a5bf12cf4274f7051bbc645ae22e5
slowbrox <raudalesdarwin@gmail.com>
2024-03-18 19:39:10 -0600

    Add project files.

---
+++ Proyecto/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
