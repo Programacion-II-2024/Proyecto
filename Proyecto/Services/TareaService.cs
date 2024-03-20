fe5e1bd36fb770474bb3acd4484293f2e9d6f2a7
slowbrox <raudalesdarwin@gmail.com>
2024-03-20 12:52:22 -0600

    Configuracion base de datos

--- Proyecto/Services/TareaService.cs
+++ Proyecto/Services/TareaService.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Proyecto.Models;
+using SQLite;
 
 namespace Proyecto.Services
 {
-    internal class TareaService
+    public class TareaService
     {
+        private readonly SQLiteAsyncConnection _dbConnection;
+
+        public TareaService()
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Tarea.db3");
+            _dbConnection = new SQLiteAsyncConnection(dbPath);
+            _dbConnection.CreateTableAsync<Tarea>().Wait();
+        }
+
+        public async Task<List<Tarea>> GetAllTareasAsync()
+        {
+            return await _dbConnection.Table<Tarea>().ToListAsync();
+        }
+
+        public async Task<int> InsertTareaAsync(Tarea tarea)
+        {
+            return await _dbConnection.InsertAsync(tarea);
+        }
+
+        public async Task<int> UpdateTareaAsync(Tarea tarea)
+        {
+            return await _dbConnection.UpdateAsync(tarea);
+        }
+
+        public async Task<int> DeleteTareaAsync(Tarea tarea)
+        {
+            return await _dbConnection.DeleteAsync(tarea);
+        }
     }
 }
